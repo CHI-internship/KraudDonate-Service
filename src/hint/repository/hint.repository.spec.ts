ece56b7b0cf6186b3461e5d9ee26e213d1e2d379
Taras Mysiv <tarasmysiv@gmail.com>
2022-12-22 15:03:22 +0200

    add test branch

--- src/hint/repository/hint.repository.spec.ts
+++ src/hint/repository/hint.repository.spec.ts
@@ -58,7 +58,7 @@ describe('Hint Repository', () => {
       info: 'new info',
     };
     test('should create hint', async () => {
-      const newHint = await hintRepository.createHint(hint, userMock().id);
+      const newHint = await hintRepository.createHint(hint, 2);
       expect(newHint).toMatchObject(HintMatchingObject);
       await prismaService.volunteer_hint.delete({ where: { id: newHint.id } });
     });
